874fca681a6937ab1f9e24bb05b579bd40cb7b6e
louiscklaw <louiscklaw@gmail.com>
2021-06-19 03:03:04 +0800

    update and test,

--- build.sh
+++ build.sh
@@ -21,36 +21,37 @@ sudo rm -rf "/Applications/R.app" &
 sudo rm -rf "/Applications/VirtualBox.app" &
 sudo rm -rf "/Applications/Visual Studio.app" &
 
-sudo rm -rf "/Applications/Xcode_10.3_beta.app" &
-sudo rm -rf "/Applications/Xcode_10.3.0.app" &
-sudo rm -rf "/Applications/Xcode_10.3.app" &
-sudo rm -rf "/Applications/Xcode_11.2.1_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.2.1.app" &
-sudo rm -rf "/Applications/Xcode_11.2.app" &
-sudo rm -rf "/Applications/Xcode_11.3.1_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.3.1.app" &
-sudo rm -rf "/Applications/Xcode_11.3.app" &
-sudo rm -rf "/Applications/Xcode_11.4.1_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.4.1.app" &
-sudo rm -rf "/Applications/Xcode_11.4.app" &
-sudo rm -rf "/Applications/Xcode_11.5_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.5.0.app" &
-sudo rm -rf "/Applications/Xcode_11.5.app" &
-sudo rm -rf "/Applications/Xcode_11.6_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.6.0.app" &
-sudo rm -rf "/Applications/Xcode_11.6.app" &
-sudo rm -rf "/Applications/Xcode_11.7_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.7.0.app" &
-sudo rm -rf "/Applications/Xcode_11.7.app" &
-sudo rm -rf "/Applications/Xcode_12.0.1.app" &
-sudo rm -rf "/Applications/Xcode_12.1.0.app" &
-# sudo rm -rf "/Applications/Xcode_12.1.1.app" &
-# sudo rm -rf "/Applications/Xcode_12.1.app" &
-# sudo rm -rf "/Applications/Xcode_12.2.0.app" &
-# sudo rm -rf "/Applications/Xcode_12.2.app" &
-# sudo rm -rf "/Applications/Xcode_12.3.0.app" &
-# sudo rm -rf "/Applications/Xcode_12.3.app" &
+sudo rm -rf "/Applications/Xcode_10.3_beta.app"
+sudo rm -rf "/Applications/Xcode_10.3.0.app"
+sudo rm -rf "/Applications/Xcode_10.3.app"
+sudo rm -rf "/Applications/Xcode_11.2.1_beta.app"
+sudo rm -rf "/Applications/Xcode_11.2.1.app"
+sudo rm -rf "/Applications/Xcode_11.2.app"
+sudo rm -rf "/Applications/Xcode_11.3.1_beta.app"
+sudo rm -rf "/Applications/Xcode_11.3.1.app"
+sudo rm -rf "/Applications/Xcode_11.3.app"
+sudo rm -rf "/Applications/Xcode_11.4.1_beta.app"
+sudo rm -rf "/Applications/Xcode_11.4.1.app"
+sudo rm -rf "/Applications/Xcode_11.4.app"
+sudo rm -rf "/Applications/Xcode_11.5_beta.app"
+sudo rm -rf "/Applications/Xcode_11.5.0.app"
+sudo rm -rf "/Applications/Xcode_11.5.app"
+sudo rm -rf "/Applications/Xcode_11.6_beta.app"
+sudo rm -rf "/Applications/Xcode_11.6.0.app"
+sudo rm -rf "/Applications/Xcode_11.6.app"
+sudo rm -rf "/Applications/Xcode_11.7_beta.app"
+sudo rm -rf "/Applications/Xcode_11.7.0.app"
+sudo rm -rf "/Applications/Xcode_11.7.app"
+sudo rm -rf "/Applications/Xcode_12.0.1.app"
+sudo rm -rf "/Applications/Xcode_12.1.0.app"
+sudo rm -rf "/Applications/Xcode_12.1.1.app"
+sudo rm -rf "/Applications/Xcode_12.1.app"
+sudo rm -rf "/Applications/Xcode_12.2.0.app"
+sudo rm -rf "/Applications/Xcode_12.2.app"
+sudo rm -rf "/Applications/Xcode_12.3.0.app"
+sudo rm -rf "/Applications/Xcode_12.3.app"
 
+ls -1 /Applications/*.app
 
 df -kh
 
