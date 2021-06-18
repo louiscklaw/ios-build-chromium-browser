8e968def4c7211806bd4d9eb0e9a34251749fe9a
louiscklaw <louiscklaw@gmail.com>
2021-06-19 03:08:34 +0800

    update and test,

--- build.sh
+++ build.sh
@@ -27,18 +27,24 @@ sudo rm -rf "/Applications/Xcode_10.3.app" &
 sudo rm -rf "/Applications/Xcode_11.2.1_beta.app" &
 sudo rm -rf "/Applications/Xcode_11.2.1.app" &
 sudo rm -rf "/Applications/Xcode_11.2.app" &
+wait
+
 sudo rm -rf "/Applications/Xcode_11.3.1_beta.app" &
 sudo rm -rf "/Applications/Xcode_11.3.1.app" &
 sudo rm -rf "/Applications/Xcode_11.3.app" &
 sudo rm -rf "/Applications/Xcode_11.4.1_beta.app" &
 sudo rm -rf "/Applications/Xcode_11.4.1.app" &
 sudo rm -rf "/Applications/Xcode_11.4.app" &
+wait
+
 sudo rm -rf "/Applications/Xcode_11.5_beta.app" &
 sudo rm -rf "/Applications/Xcode_11.5.0.app" &
 sudo rm -rf "/Applications/Xcode_11.5.app" &
 sudo rm -rf "/Applications/Xcode_11.6_beta.app" &
 sudo rm -rf "/Applications/Xcode_11.6.0.app" &
 sudo rm -rf "/Applications/Xcode_11.6.app" &
+wait
+
 sudo rm -rf "/Applications/Xcode_11.7_beta.app" &
 sudo rm -rf "/Applications/Xcode_11.7.0.app" &
 sudo rm -rf "/Applications/Xcode_11.7.app" &
@@ -46,10 +52,14 @@ sudo rm -rf "/Applications/Xcode_12.0.1.app" &
 sudo rm -rf "/Applications/Xcode_12.1.0.app" &
 sudo rm -rf "/Applications/Xcode_12.1.1.app" &
 sudo rm -rf "/Applications/Xcode_12.1.app" &
+wait
+
 sudo rm -rf "/Applications/Xcode_12.2.0.app" &
 sudo rm -rf "/Applications/Xcode_12.2.app" &
 sudo rm -rf "/Applications/Xcode_12.3.0.app" &
 sudo rm -rf "/Applications/Xcode_12.3.app" &
+wait
+
 
 ls -1 /Applications/*.app
 
