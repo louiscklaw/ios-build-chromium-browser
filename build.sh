8949f684beef92988180b8d2fb6016f5027596f3
louiscklaw <louiscklaw@gmail.com>
2021-06-19 01:53:19 +0800

    update and test,

--- build.sh
+++ build.sh
@@ -13,27 +13,34 @@ brew install watch
 
 sudo rm -rf /Applications/Firefox.app
 sudo rm -rf "/Applications/Google Chrome.app" &
+sudo rm -rf "/Applications/Julia-1.6.app" &
+sudo rm -rf "/Applications/Microsoft Edge.app" &
+sudo rm -rf "/Applications/PowerShell.app" &
+sudo rm -rf "/Applications/R.app" &
+sudo rm -rf "/Applications/VirtualBox.app" &
+sudo rm -rf "/Applications/Visual Studio.app" &
+
 sudo rm -rf "/Applications/Xcode_10.3.0.app" &
 sudo rm -rf "/Applications/Xcode_10.3.app" &
 sudo rm -rf "/Applications/Xcode_10.3_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.2.1.app" &
-sudo rm -rf "/Applications/Xcode_11.2.1_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.2.app" &
-sudo rm -rf "/Applications/Xcode_11.3.1.app" &
-sudo rm -rf "/Applications/Xcode_11.3.1_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.3.app" &
-sudo rm -rf "/Applications/Xcode_11.4.1.app" &
-sudo rm -rf "/Applications/Xcode_11.4.1_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.4.app" &
-sudo rm -rf "/Applications/Xcode_11.5.0.app" &
-sudo rm -rf "/Applications/Xcode_11.5.app" &
-sudo rm -rf "/Applications/Xcode_11.5_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.6.0.app" &
-sudo rm -rf "/Applications/Xcode_11.6.app" &
-sudo rm -rf "/Applications/Xcode_11.6_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.7.0.app" &
-sudo rm -rf "/Applications/Xcode_11.7.app" &
-sudo rm -rf "/Applications/Xcode_11.7_beta.app" &
+# sudo rm -rf "/Applications/Xcode_11.2.1.app" &
+# sudo rm -rf "/Applications/Xcode_11.2.1_beta.app" &
+# sudo rm -rf "/Applications/Xcode_11.2.app" &
+# sudo rm -rf "/Applications/Xcode_11.3.1.app" &
+# sudo rm -rf "/Applications/Xcode_11.3.1_beta.app" &
+# sudo rm -rf "/Applications/Xcode_11.3.app" &
+# sudo rm -rf "/Applications/Xcode_11.4.1.app" &
+# sudo rm -rf "/Applications/Xcode_11.4.1_beta.app" &
+# sudo rm -rf "/Applications/Xcode_11.4.app" &
+# sudo rm -rf "/Applications/Xcode_11.5.0.app" &
+# sudo rm -rf "/Applications/Xcode_11.5.app" &
+# sudo rm -rf "/Applications/Xcode_11.5_beta.app" &
+# sudo rm -rf "/Applications/Xcode_11.6.0.app" &
+# sudo rm -rf "/Applications/Xcode_11.6.app" &
+# sudo rm -rf "/Applications/Xcode_11.6_beta.app" &
+# sudo rm -rf "/Applications/Xcode_11.7.0.app" &
+# sudo rm -rf "/Applications/Xcode_11.7.app" &
+# sudo rm -rf "/Applications/Xcode_11.7_beta.app" &
 # sudo rm -rf "/Applications/Xcode_12.0.1.app" &
 # sudo rm -rf "/Applications/Xcode_12.1.0.app" &
 # sudo rm -rf "/Applications/Xcode_12.1.1.app" &
@@ -43,12 +50,7 @@ sudo rm -rf "/Applications/Xcode_11.7_beta.app" &
 # sudo rm -rf "/Applications/Xcode_12.3.0.app" &
 # sudo rm -rf "/Applications/Xcode_12.3.app" &
 
-sudo rm -rf "/Applications/Julia-1.6.app" &
-sudo rm -rf "/Applications/Microsoft Edge.app" &
-sudo rm -rf "/Applications/PowerShell.app" &
-sudo rm -rf "/Applications/R.app" &
-sudo rm -rf "/Applications/VirtualBox.app" &
-sudo rm -rf "/Applications/Visual Studio.app" &
+
 
 wait
 exit 1
