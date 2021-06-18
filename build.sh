1b5db66d61b68f7478cceffd7d6ed30af22f285a
louiscklaw <louiscklaw@gmail.com>
2021-06-19 02:46:29 +0800

    update and test,

--- build.sh
+++ build.sh
@@ -58,14 +58,14 @@ mkdir -p workdir
 
 pushd workdir
 
-  git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  # git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 
-  export PATH="$PATH:$PWD/depot_tools"
+  # export PATH="$PATH:$PWD/depot_tools"
 
-  which fetch
-  # /Users/runner/work/fastmac/fastmac/workdir/depot_tools/fetch
+  # which fetch
+  # # /Users/runner/work/fastmac/fastmac/workdir/depot_tools/fetch
 
-  fetch --no-history ios &
+  # fetch --no-history ios &
 
 
 wait
