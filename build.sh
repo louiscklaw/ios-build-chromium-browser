f97298e58a342a392d5fb6b762a6763be79f93dd
louiscklaw <louiscklaw@gmail.com>
2021-06-19 01:59:31 +0800

    update and test,

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ sudo rm -rf "/Applications/VirtualBox.app" &
 sudo rm -rf "/Applications/Visual Studio.app" &
 
 sudo rm -rf "/Applications/Xcode_10.3.0.app" &
-# sudo rm -rf "/Applications/Xcode_10.3.app" &
+sudo rm -rf "/Applications/Xcode_10.3.app" &
 # sudo rm -rf "/Applications/Xcode_10.3_beta.app" &
 # sudo rm -rf "/Applications/Xcode_11.2.1.app" &
 # sudo rm -rf "/Applications/Xcode_11.2.1_beta.app" &
