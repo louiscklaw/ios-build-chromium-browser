1fac0e4ee77ed3f2b302c5287b89e358ccece22c
louiscklaw <louiscklaw@gmail.com>
2021-06-19 09:51:55 +0800

    update and test,

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ brew install iftop
 brew install magic-wormhole
 
 
+./clean_applications.sh &
 
 ls -1 /Applications/*.app
 
@@ -35,77 +36,7 @@ pushd workdir
   pushd src
 
     ios/build/tools/setup-gn.py
-    autoninja -C out/Debug-iphonesimulator gn_all &
-
-
-
-
-
-
-sudo rm -rf "/Applications/Firefox.app" &
-sudo rm -rf "/Applications/Google Chrome.app" &
-sudo rm -rf "/Applications/Julia-1.6.app" &
-sudo rm -rf "/Applications/Microsoft Edge.app" &
-sudo rm -rf "/Applications/PowerShell.app" &
-sudo rm -rf "/Applications/R.app" &
-sudo rm -rf "/Applications/VirtualBox.app" &
-sudo rm -rf "/Applications/Visual Studio.app" &
-
-sudo rm -rf "/Applications/Xcode_10.3_beta.app" &
-sudo rm -rf "/Applications/Xcode_10.3.0.app" &
-sudo rm -rf "/Applications/Xcode_10.3.app" &
-wait
-
-sudo rm -rf "/Applications/Xcode_11.2.1_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.2.1.app" &
-sudo rm -rf "/Applications/Xcode_11.2.app" &
-wait
-
-sudo rm -rf "/Applications/Xcode_11.3.1_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.3.1.app" &
-sudo rm -rf "/Applications/Xcode_11.3.app" &
-wait
-
-sudo rm -rf "/Applications/Xcode_11.4.1_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.4.1.app" &
-sudo rm -rf "/Applications/Xcode_11.4.app" &
-wait
-
-sudo rm -rf "/Applications/Xcode_11.5_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.5.0.app" &
-sudo rm -rf "/Applications/Xcode_11.5.app" &
-wait
-
-sudo rm -rf "/Applications/Xcode_11.6_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.6.0.app" &
-sudo rm -rf "/Applications/Xcode_11.6.app" &
-wait
-
-sudo rm -rf "/Applications/Xcode_11.7_beta.app" &
-sudo rm -rf "/Applications/Xcode_11.7.0.app" &
-sudo rm -rf "/Applications/Xcode_11.7.app" &
-wait
-
-sudo rm -rf "/Applications/Xcode_12.0.1.app" &
-sudo rm -rf "/Applications/Xcode_12.1.0.app" &
-sudo rm -rf "/Applications/Xcode_12.1.1.app" &
-sudo rm -rf "/Applications/Xcode_12.1.app" &
-wait
-
-sudo rm -rf "/Applications/Xcode_12.2.0.app" &
-sudo rm -rf "/Applications/Xcode_12.2.app" &
-wait
-
-sudo rm -rf "/Applications/Xcode_12.3.0.app" &
-sudo rm -rf "/Applications/Xcode_12.3.app" &
-wait
-
-
-
-
-
-
-
+    autoninja -C out/Debug-iphonesimulator gn_all
 
   popd
 
---
+++ clean_applications.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -ex
+
+sudo rm -rf "/Applications/Firefox.app" &
+sudo rm -rf "/Applications/Google Chrome.app" &
+sudo rm -rf "/Applications/Julia-1.6.app" &
+sudo rm -rf "/Applications/Microsoft Edge.app" &
+sudo rm -rf "/Applications/PowerShell.app" &
+sudo rm -rf "/Applications/R.app" &
+sudo rm -rf "/Applications/VirtualBox.app" &
+sudo rm -rf "/Applications/Visual Studio.app" &
+
+sudo rm -rf "/Applications/Xcode_10.3_beta.app" &
+sudo rm -rf "/Applications/Xcode_10.3.0.app" &
+sudo rm -rf "/Applications/Xcode_10.3.app" &
+wait
+
+sudo rm -rf "/Applications/Xcode_11.2.1_beta.app" &
+sudo rm -rf "/Applications/Xcode_11.2.1.app" &
+sudo rm -rf "/Applications/Xcode_11.2.app" &
+wait
+
+sudo rm -rf "/Applications/Xcode_11.3.1_beta.app" &
+sudo rm -rf "/Applications/Xcode_11.3.1.app" &
+sudo rm -rf "/Applications/Xcode_11.3.app" &
+wait
+
+sudo rm -rf "/Applications/Xcode_11.4.1_beta.app" &
+sudo rm -rf "/Applications/Xcode_11.4.1.app" &
+sudo rm -rf "/Applications/Xcode_11.4.app" &
+wait
+
+sudo rm -rf "/Applications/Xcode_11.5_beta.app" &
+sudo rm -rf "/Applications/Xcode_11.5.0.app" &
+sudo rm -rf "/Applications/Xcode_11.5.app" &
+wait
+
+sudo rm -rf "/Applications/Xcode_11.6_beta.app" &
+sudo rm -rf "/Applications/Xcode_11.6.0.app" &
+sudo rm -rf "/Applications/Xcode_11.6.app" &
+wait
+
+sudo rm -rf "/Applications/Xcode_11.7_beta.app" &
+sudo rm -rf "/Applications/Xcode_11.7.0.app" &
+sudo rm -rf "/Applications/Xcode_11.7.app" &
+wait
+
+sudo rm -rf "/Applications/Xcode_12.0.1.app" &
+sudo rm -rf "/Applications/Xcode_12.1.0.app" &
+sudo rm -rf "/Applications/Xcode_12.1.1.app" &
+sudo rm -rf "/Applications/Xcode_12.1.app" &
+wait
+
+sudo rm -rf "/Applications/Xcode_12.2.0.app" &
+sudo rm -rf "/Applications/Xcode_12.2.app" &
+wait
+
+sudo rm -rf "/Applications/Xcode_12.3.0.app" &
+sudo rm -rf "/Applications/Xcode_12.3.app" &
+wait
